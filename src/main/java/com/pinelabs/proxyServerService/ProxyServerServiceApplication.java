6850604ab9b0a6679b3e2001e2341cbfac71c878
Rahul Agarwal <rahul.agarwal@pinelabs.com>
2021-12-15 16:42:11 +0530

    Code review Changes

--- src/main/java/com/pinelabs/proxyServerService/ProxyServerServiceApplication.java
+++ src/main/java/com/pinelabs/proxyServerService/ProxyServerServiceApplication.java
@@ -7,24 +7,28 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
 
+import com.pinelabs.proxyServerService.logger.LoggerClass;
+
 @SpringBootApplication
 public class ProxyServerServiceApplication {
-	
+
 	@Autowired
 	ProxyServerService proxyServerService;
-	
+
 	public static void main(String[] args) {
 		SpringApplication.run(ProxyServerServiceApplication.class, args);
-		
 	}
 
 	@EventListener(ApplicationReadyEvent.class)
-	public void startProxyServer() {	    
+	public void startProxyServer() {
 		try {
 			proxyServerService.startProxyServer();
-		} catch (TException e) {
-			e.printStackTrace();
+		} 
+		catch (TException e) {
+			LoggerClass.LogMessage(LoggerClass.eMessageType.MT_INFORMATION, "startProxyServer Exception: " + e.getMessage());
 		}
-	    
+		catch(Exception ex) {
+			LoggerClass.LogMessage(LoggerClass.eMessageType.MT_INFORMATION, "startProxyServer Exception: " + ex.getMessage());
+		}
 	}
 }
